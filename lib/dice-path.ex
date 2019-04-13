c5972ba0334139717dfdb725b217199ba90944e9
Ivan Danilychev <eveningsteps@gmail.com>
2019-04-13 22:33:27 +0300

    finally, beat "Dice Path"

--- lib/dice-path.ex
+++ lib/dice-path.ex
@@ -6,7 +6,7 @@ defmodule HackerRank.DicePath do
 
     defstruct top: 1, front: 2, left: 3
 
-    def right(dice) do
+    def to_right(dice) do
       %Dice{
         top: dice.left,
         front: dice.front,
@@ -21,22 +21,75 @@ defmodule HackerRank.DicePath do
         left: dice.left
       }
     end
-
-    def up(dice), do: dice |> down() |> down()
-    def left(dice), do: dice |> right() |> right()
   end
 
-  def to_right(_) do
-  end
-  def to_down(_) do
+  defmodule State do
+    def start_link() do
+      Agent.start_link(
+        fn ->
+          %{{1, 1} => MapSet.new([{%Dice{}, %Dice{}.top}])}
+        end,
+        name: __MODULE__
+      )
+    end
+
+    def solve(m, n) do
+      case Agent.get(__MODULE__, &Map.fetch(&1, {m, n})) do
+        {:ok, val} -> val
+        :error ->
+          new_val = case {m, n} do
+            {1, _} -> solve(1, n - 1) |> HackerRank.DicePath.to_right()
+            {_, 1} -> solve(m - 1, 1) |> HackerRank.DicePath.down()
+            _ -> MapSet.union(
+              solve(m, n - 1) |> HackerRank.DicePath.to_right(),
+              solve(m - 1, n) |> HackerRank.DicePath.down()
+            )
+          end
+          Agent.update(__MODULE__, &Map.put(&1, {m, n}, new_val))
+          new_val
+      end
+    end
   end
 
-  def solve(1, 1), do: {%Dice{}, %Dice{}, %Dice{}.top}
-  def solve(1, n), do: solve(1, n - 1) |> to_right()
-  def solve(m, 1), do: solve(m - 1, 1) |> to_down()
-
-  def solve(_, _) do
+  def to_right(mapset) when is_map(mapset) do
+      mapset |> Enum.map(&to_right/1) |> MapSet.new()
   end
 
-  def max_sum(m, n), do: solve(m, n) |> elem(1)
+  def to_right({dice, score}) do
+    new_dice = dice |> Dice.to_right()
+    {new_dice, score + new_dice.top}
+  end
+
+  def down(mapset) when is_map(mapset) do
+    mapset |> Enum.map(&down/1) |> MapSet.new()
+  end
+
+  def down({dice, score}) do
+    new_dice = dice |> Dice.down()
+    {new_dice, score + new_dice.top}
+  end
+
+  def solve(m, n) do
+    HackerRank.DicePath.State.solve(m, n)
+      |> Enum.map(&elem(&1, 1)) |> Enum.max()
+  end
+
+  def init() do
+    HackerRank.DicePath.State.start_link()
+  end
+
+  def main() do
+    init()
+    _ = IO.read(:line)
+    IO.read(:all)
+    |> String.split("\n", trim: true)
+    |> Enum.map(
+      fn line ->
+        processed = line |> String.split(" ") |> Enum.map(&String.to_integer/1)
+        apply(&solve/2, processed)
+      end
+    )
+    |> Enum.join("\n")
+    |> IO.puts()
+  end
 end
--- test/dice-path-test.ex
+++ test/dice-path-test.ex
@@ -7,7 +7,7 @@ defmodule HackerRank.DicePath.Test do
     initial = %Dice{}
 
     assert initial |> Dice.down() |> Dice.down() |> Dice.down() |> Dice.down() == initial
-    assert initial |> Dice.right() |> Dice.right() |> Dice.right() |> Dice.right() == initial
+    assert initial |> Dice.to_right() |> Dice.to_right() |> Dice.to_right() |> Dice.to_right() == initial
 
     rotated = initial |> Dice.down()
 
@@ -15,24 +15,24 @@ defmodule HackerRank.DicePath.Test do
     assert rotated.front == initial.top
     assert rotated.left == initial.left
 
-    rotated = initial |> Dice.right()
+    rotated = initial |> Dice.to_right()
 
     assert rotated.top == initial.left
     assert rotated.left == Dice.pip_sum() - initial.top
     assert rotated.front == initial.front
 
-    rotated = initial |> Dice.right() |> Dice.down()
+    rotated = initial |> Dice.to_right() |> Dice.down()
 
     assert rotated.top == Dice.pip_sum() - initial.front
     assert rotated.left == Dice.pip_sum() - initial.top
     assert rotated.front == initial.left
   end
 
-  @tag skip: "No functionality yet"
   test "simple" do
-    assert HackerRank.DicePath.max_sum(2, 2) == 9
-    assert HackerRank.DicePath.max_sum(1, 2) == 4
-    assert HackerRank.DicePath.max_sum(2, 1) == 6
-    assert HackerRank.DicePath.max_sum(3, 3) == 19
+    HackerRank.DicePath.init()
+    assert HackerRank.DicePath.solve(2, 2) == 9
+    assert HackerRank.DicePath.solve(1, 2) == 4
+    assert HackerRank.DicePath.solve(2, 1) == 6
+    assert HackerRank.DicePath.solve(3, 3) == 19
   end
 end
